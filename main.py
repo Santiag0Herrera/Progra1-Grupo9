19adbb9ebc33aba2e6e3683b601e79e0abdf1ac9
Nicolas <nico2016@gmail.com>
2024-10-19 13:27:59 -0300

    Metodo + Prueba de json de PoblacionXProvincia
    Se modificael json para fixear problemas de caracteres especiales (tilde) en los print por utf-8

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def main():
     partido = jsonCandidates[i]["partido"]
     print(f"{candidato} --> {partido}")
 
+    #Prueba de json de Provincias
+    print("\n############ POBLACION X PROVINCIA ############\n") # Added a newline character
+
+    jsonProvincias = getJson('../data/provinces.json')
+    for provincia in jsonProvincias:
+        print(provincia + " --> " , jsonProvincias[provincia]["poblacion"])
+
 if __name__=="__main__":
   main()
