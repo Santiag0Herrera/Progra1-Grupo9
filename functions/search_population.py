d299eb31e700fae58777a8aefa1e2292e0cac757
Nicolas Villarreal <nvillarreal@sms-sudamerica.com>
2024-10-29 20:06:06 -0300

    Eliminado comentario de print.generator

--- functions/search_population.py
+++
@@ -1,6 +0,0 @@
-# Obtener la población de una provincia
-def buscar_poblacion(provincia, jsonProvinces):
-    if provincia in jsonProvinces:
-        return jsonProvinces[provincia]["población"]
-    else:
-        return f'La provincia "{provincia}" no se encuentra en el registro.'
--- functions/time_line_candidates.py
+++
@@ -1,5 +0,0 @@
-def actualizar_json_candidatos(json_anterior):
-  provinces = json_anterior
-  for prov in provinces:
-    # print(prov)
-    print(json_anterior[prov]["peso"])
