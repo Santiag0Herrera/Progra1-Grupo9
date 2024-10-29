84089ef1cebc974fc3c4b68df33170076da2083d
Santiago <santi.nico.herrera@gmail.com>
2024-10-29 19:59:02 -0300

    Purgatory

--- functions/candidatos.py
+++
@@ -1,8 +0,0 @@
-# Función para obtener el partido de un candidato
-def obtener_partido_candidato(jsonCandidates, candidato):
-    try:
-        # Obtiene el partido del candidato
-        partido = jsonCandidates[candidato]["partido"]
-        return partido
-    except KeyError:
-        return "Candidato no encontrado. Verifica el nombre del candidato."
--- functions/data_validation.py
+++
@@ -1,5 +0,0 @@
-def validar_string(candidato):
-  if len(candidato.strip()) == 0:
-    return False
-  else:
-    return True
